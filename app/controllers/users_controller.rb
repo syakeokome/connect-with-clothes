c60bc99e47de09d645f9dcbe577e4930b5589535
syakeokome <meoko4635@gmail.com>
2022-03-11 16:24:19 +0000

    mypage edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,32 @@ class UsersController < ApplicationController
   end
 
   def mypage
+    @user = current_user
   end
 
   def edit
+   @user =  current_user
   end
 
   def unscribe
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+     redirect_to user_mypage_path
+    else
+     render :edit
+    end
   end
 
   def withdraw
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:nickname, :user_image, :email, :name, :self_introduction, :postal_code, :address, :age, :height, :is_active)
   end
 
 end
