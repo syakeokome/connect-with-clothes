d4ea9366a8cbd4dbf88ea9a860ec550bb6df03a8
syakeokome <meoko4635@gmail.com>
2022-03-19 08:34:49 +0000

    item show link_to

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,7 @@ class ItemsController < ApplicationController
     # 閲覧数表示
     #impressionist(@item, nil, :unique => ["session_hash"])
     @comment = Comment.new
+    @user = @item.user
   end
 
   def create
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,9 +13,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.save
+    redirect_to orders_comfirm_path
   end
 
   def comfirm
+  end
+
+  private
+
+  def order_params
+   params.require(:order).permit(:name, :postal_code, :address, :price, :payment_method)
   end
 
 end
